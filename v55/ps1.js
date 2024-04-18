f0f76ef1b0cd6c27d9881de8a4698d300c14915b
Aayush Golecha <62604015+AayushGolecha@users.noreply.github.com>
2024-04-18 22:29:47 +0530

    v55 completed

---
+++ v55/ps1.js
@@ -0,0 +1,32 @@
+// Q!
+var str="Aayush"
+str= str+1
+console.log(str)
+
+// Q2
+console.log(typeof str)
+
+//Q3
+const o={
+    name:"Aayush",
+    Id:55
+}
+console.log(o)
+// o=5
+console.log(o)
+
+//Q4
+o.salary=40000
+console.log(o)
+
+//Q5
+let dict={
+    "a":"a",
+    "b":"b",
+    "c":"c",
+    "d":"d",
+    "e":"e"
+}
+console.log(dict)
+console.log(dict['a'])
+console.log(dict.c)
---
+++ v55/script.js
@@ -0,0 +1,50 @@
+console.log("Hello World")
+
+var a= 5
+a=a+1
+var b= 6
+console.log(a+b+8)
+var c="Aayush"
+
+console.log(c)
+
+{
+    let a=4
+    console.log(a)
+}
+console.log(a)
+
+const d=55
+// d=d+5
+console.log(d)
+
+let p=22
+let q="Aayush"
+let r=5.55
+let s=null
+let t=true
+let u=undefined
+let v
+console.log(p, q, r, s, t, u, v)
+console.log(typeof p, typeof q, typeof r, typeof s, typeof t,typeof  u, typeof v)
+
+let obj={
+    Name: "Aayush",
+    "Job Code":5500
+}
+console.log(obj)
+obj.salary="5LPA"
+console.log(obj)
+obj.salary="10LPA"
+console.log(obj)
+obj["Job Code"]=500
+console.log(obj)
+
+
+// Quick Quiz
+let student={
+    name: "Rohan",
+    "phone-number":9876543210,
+    marks:80
+}
+console.log(student)
