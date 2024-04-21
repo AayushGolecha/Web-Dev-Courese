581759ab6be19ab3fcf975fb5c84b96a8a36b4ed
Aayush Golecha <62604015+AayushGolecha@users.noreply.github.com>
2024-04-21 23:22:13 +0530

    v60 completed

---
+++ v60/index.js
@@ -0,0 +1,38 @@
+console.log("This is string tutorial")
+
+let a = "Aayush"
+console.log(a)
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a[6])
+
+let bf = "Aayush"
+let gf = "Priya"
+console.log("The name of " + bf + " girlfriend is " + gf)
+console.log(`The name of ${bf} girlfriend is ${gf}`)
+
+let b = "Payal"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1))
+console.log(b.slice(1, 4))
+console.log(b.replace("a", "o"))
+console.log(b.concat("Weds", a))
+console.log(b.charAt(3))
+console.log(b.indexOf("y"))
+
+let c = "  Karan  "
+console.log(c)
+console.log(c.trim())
+
+let fruit1 = "Banana\'s"
+console.log(fruit1)
+let fruit2 = "Banana\"s"
+console.log(fruit2)
+let fruit3 = "Banana\ns"
+console.log(fruit3)
---
+++ v60/ps4.js
@@ -0,0 +1,20 @@
+// Q1
+console.log("Aayush\"".length)
+
+// Q2
+let a= "Aayush"
+console.log(a.includes("ay"))
+console.log(a.includes("yu",2))
+console.log(a.includes("yu",3))
+
+// Q3
+let b=prompt("Enter the string")
+console.log(b.toLowerCase())
+
+// Q4
+let str="Please give rs 1000"
+console.log(str.slice(15))
+
+// Q5
+let c="Iamfine"
+console.log(c.replace("i","a"))
