c7bf37d58fb3f6880c9fa18eefd6e51b287b61b2
Aayush Golecha <62604015+AayushGolecha@users.noreply.github.com>
2024-04-24 12:16:33 +0530

    v65 updated

--- v65/index.js
+++ v65/index.js
@@ -9,3 +9,11 @@ let fact=arr.reduce((x,y)=>{
     return x*y
 })
 console.log(`Factorial of ${num} is ${fact}`)
+
+let n=5
+function factorial(number){
+    let arr = Array.from(Array(number+1).keys())
+    let c = arr.slice(1,).reduce((a, b)=> a*b)
+    return c
+ }
+ console.log(factorial(n))
