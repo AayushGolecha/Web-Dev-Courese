79f6f380f7e421be038ec5eb111de1e5ce7a233c
Aayush Golecha <62604015+AayushGolecha@users.noreply.github.com>
2024-05-11 23:10:39 +0530

    v84  updated

--- v84/script.js
+++ v84/script.js
@@ -1,8 +1,1 @@
-const scrollbar = document.querySelector('.scrollbar');
-const thumb = scrollbar.querySelector('.scrollbar-thumb');
 
-scrollbar.addEventListener('scroll', () => {
-  const scrollPercentage = scrollbar.scrollTop / (scrollbar.scrollHeight - scrollbar.clientHeight);
-  const thumbPosition = scrollPercentage * (scrollbar.clientHeight - thumb.clientHeight);
-  thumb.style.transform = `translateY(${thumbPosition}px)`;
-});
